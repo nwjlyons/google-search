56a244d442d29b046a7134776393596a8bf849d6
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2014-06-14 22:35:45 +0300

    Implements the selection support before selecting the word.

--- google_search.py
+++ google_search.py
@@ -5,11 +5,22 @@ class GoogleSearchCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
 
-        selection = ""
+        querys = []
         for region in self.view.sel():
-            word = self.view.word(region)
-            if not word.empty():
-                selection += " " + self.view.substr(word)
+            if region.empty():
+                # if we have no selection grab the current word
+                word = self.view.word(region)
+                if not word.empty():
+                    querys.append(self.view.substr(word))
+            else:
+                # append the selection
+                if not region.empty():
+                    querys.append(self.view.substr(region))
 
-        webbrowser.open("http://www.google.com/search?q=%s" % selection)
+
+        if len(querys) != 0:
+            selection = " ".join(querys)
+            webbrowser.open("http://www.google.com/search?q=%s" % selection)
+        else:
+            print("Google Search Plugin: Nothing to search !")
 
