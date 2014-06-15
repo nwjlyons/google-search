ce24edcbe1c625326ba94f60ced1ee93328bdeae
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2014-06-15 12:28:31 +0300

    Added the search all command

--- google_search.py
+++ google_search.py
@@ -22,5 +22,32 @@ class GoogleSearchCommand(sublime_plugin.TextCommand):
             selection = " ".join(querys)
             webbrowser.open("http://www.google.com/search?q=%s" % selection)
         else:
+            sublime.status_message(" Nothing to search !")
             print("Google Search Plugin: Nothing to search !")
 
+
+class GoogleSearchAnyCommand(sublime_plugin.WindowCommand):
+    """
+    A command that prompts the user to enter search query text.
+    """
+
+    def run(self):
+
+        def show_input_panel():
+            self.window.show_input_panel(
+                'Enter a search query',
+                '',
+                self.on_done,
+                self.on_change,
+                self.on_cancel
+                )
+        sublime.set_timeout(show_input_panel, 10)
+
+    def on_done(self, text):
+         webbrowser.open("http://www.google.com/search?q=%s" % text)
+
+    def on_change(self):
+        pass
+
+    def on_cancel(self):
+        pass
